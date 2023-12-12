a462d1ebb769b7eec652b9fe55ec2c371ea38a40
Deborah <deborag.g17@gmail.com>
2023-12-12 02:14:11 -0300

    helloworld

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.generation.helloworld.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +18,18 @@ public class HelloController {
 		
 		}
 	
-		@GetMapping ("/top")
-		public String  top () {
-			return "<b>Turma TOP!!! </b>";
+		@GetMapping ("/BsmList")
+		public List<String>  BsmList () {
+			return Arrays.asList("BSM1: Comunicação" , "BSM2:Proatividade", "BSM3: Trabalho em Equipe");
+			
+		}
+			
+			@GetMapping ("/Metas")
+			public List<String>  MetasList () {
+				return Arrays.asList(
+						"\"Aprender Spring Boot\",\r\n",
+						  "Melhorar habilidades em Java\",\r\n",
+					      "Compreender endpoints em uma aplicação web\"\r\n");
 			
 	}
 }
